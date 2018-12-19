0086341dd3402b592d2f347534ea5138ac694a30
Franco Valerio <franco.gvp@hotmail.com>
2018-12-18 22:00:44 -0300

    Fix error en app.module y packages faltantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     UiModule,
     FormsModule,
     HttpClientModule,
-    LeafletModule.forRoot()
+    LeafletModule.forRoot(),
     MatNativeDateModule,
     BrowserAnimationsModule
   ],
